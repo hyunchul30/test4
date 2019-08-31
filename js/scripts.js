5d67a9e3758b738c7507318fad35adc67cae53d6
Student <example@example.com>
2019-08-31 13:48:57 -0700

    test4

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,36 @@
 function OrderPizza() {
+  this.OrderPizza=calculateTotal();
   this.pizza= pizza;
   this.cheese= cheese;
   this.meats= meats;
-  this.price= price;
+  this.calculateTotal= calculateTotal();
 };
 
-OrderPizza.prototype.totalPrice= function() {}
-if(this.pizza === "Small" && this.cheese === "Cheddar" && this.meats === "Sausage") {
+var calculateTotal= new Array();
+calculateTotal["Small"]=1;
+calculateTotal["Medium"]=2 ;
+calculateTotal["Large"]= 3;
 
-  return this.total);
+calculateTotal["Cheddar"]= 4;
+calculateTotal["ExtraCheese"]=5 ;
+calculateTotal["Feta"]= 6;
+
+calculateTotal["Sausage"]= 7;
+calculateTotal["Bacon"]= 8;
+calculateTotal["Beef"]= 9;
+OrderPizza.prototype.calculateTotal= function() {
+alert("total:"+ val)};
+  cosole.log("calculateTotal()");
+if(this.pizza == "Small" && this.cheese == "Cheddar" && this.meats == "Bacon") {
+
+  return this.calculateTotal();
 }
 
-
-
 }
-
-
-var Small = 5;
-var Medium = 8;
-var Large = 10;
-var Total = Small + Medium + Large;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 $(document).ready(function(){
   $("#OrderPizza").click(function(){
-
-  })
+$("#SZ")
+  });
 });
