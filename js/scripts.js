94ebfafd5a7939d726ec8491997b836bbe2d2665
Student <example@example.com>
2019-08-31 07:26:04 -0700

    test4

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,43 @@
+function OrderPizza() {
+  this.pizza= pizza;
+  this.cheese= cheese;
+  this.meats= meats;
+  this.price= price;
+};
+
+OrderPizza.prototype.totalPrice= function() {}
+if(this.pizza === "Small" && this.cheese === "Cheddar" && this.meats === "Sausage") {
+
+  return this.total);
+}
+
+
+
+}
+
+
+var Small = 5;
+var Medium = 8;
+var Large = 10;
+var Total = Small + Medium + Large;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 $(document).ready(function(){
-  $("").click(function(){
+  $("#OrderPizza").click(function(){
 
   })
 });
