8f61e2a925b8da895095a14ae1472112e8c126c4
Student <example@example.com>
2019-09-03 07:36:54 -0700

    test4

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,81 @@
-function OrderPizza() {
-  this.OrderPizza=calculateTotal();
+function OrderPizzal(pizza,cheese,meats){
   this.pizza= pizza;
   this.cheese= cheese;
   this.meats= meats;
-  this.calculateTotal= calculateTotal();
-};
-
-var calculateTotal= new Array();
-calculateTotal["Small"]=1;
-calculateTotal["Medium"]=2 ;
-calculateTotal["Large"]= 3;
-
-calculateTotal["Cheddar"]= 4;
-calculateTotal["ExtraCheese"]=5 ;
-calculateTotal["Feta"]= 6;
-
-calculateTotal["Sausage"]= 7;
-calculateTotal["Bacon"]= 8;
-calculateTotal["Beef"]= 9;
-OrderPizza.prototype.calculateTotal= function() {
-alert("total:"+ val)};
-  cosole.log("calculateTotal()");
-if(this.pizza == "Small" && this.cheese == "Cheddar" && this.meats == "Bacon") {
-
-  return this.calculateTotal();
-}
 
 }
 
+
+OrderPizza.prototype= function() {
+  // var calculateTotal= this.calculateTotal;
+  var OrderPizza =0;
+var total = this.size == 1; this.cheese == 4; this.meats == 9;
+return this.total;
+}
 
 $(document).ready(function(){
-  $("#OrderPizza").click(function(){
-$("#SZ")
+  $("form#OrderPizza").submit(function(){
+    event.preventDefault();
+
+var total= new total();
+total["SelectSize"]=0;
+ total["Small"]=1;
+  total["Medium"]=2 ;
+   total["Large"]= 3;
+
+ total= new total();
+ total["SelectCheese"]=0;
+  total["Cheddar"]= 4;
+   total["ExtraCheese"]=5 ;
+    total["Feta"]= 6;
+
+ total= new total();
+ total["SelectMeats"]=0;
+  total["Sausage"]= 8;
+   total["Bacon"]= 9;
+    total["Beef"]= 10;
+
+})
+})
+
+function Pizza (size,toppings1,toppings2,toppings3){
+this.delivery = delivery
+this.size = size;
+this.toppings1 = toppings1;
+this.toppings2 = toppings2;
+this.toppings3 = toppings3;
+}
+
+Pizza.prototype.total = function(){
+  var total =  + this.size + this.toppings1 + this.toppings2 + this.toppings3;
+  return total;
+}
+
+Pizza.prototype.total2 =function(){
+  var total2 =  "Your total is: $" + parseInt(this.size + this.toppings1 + this.toppings2 + this.toppings3);
+  return total2;
+}
+//front end
+
+$(document).ready(function(){
+  $("form#YourOrder").submit(function(){
+    event.preventDefault();
+
+
+    var size = parseInt($("select#YourSize").val());
+    var toppings1 = parseInt($("select#toppings1").val());
+    var toppings2 = parseInt($("select#toppings2").val());
+    var toppings3 = parseInt($("select#toppings3").val());
+
+
+var userPizza = new Pizza (size, toppings1, toppings2, toppings3, total);
+var userpizza2 = new Pizza (size, toppings1, toppings2, toppings3, total);
+  var total =  userPizza.total();
+  var total2 = userPizza.total2();
+
+
+$("#output").text(total2);
+
+
   });
 });
